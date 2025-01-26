c3457743644cc98fb9720874ca271c3b178b4a5c
muxi <yangqinxue333@163.com>
2025-01-26 23:53:18 +0000

    Run Spring boot on port 8081

---
+++ backend/db-pcr-backend/src/main/java/ic/ac/uk/db_pcr_backend/HomeController.java
@@ -0,0 +1,13 @@
+package ic.ac.uk.db_pcr_backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+  
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Spring Boot!";
+    }
+}
