a72a49f3dcc34f1c77f121c04abcfac515857edd
muxi <yangqinxue333@163.com>
2025-02-03 15:15:24 +0000

    GetModifiedFileMapping

--- backend/db-pcr-backend/src/main/java/ic/ac/uk/db_pcr_backend/controller/GerritController.java
+++ backend/db-pcr-backend/src/main/java/ic/ac/uk/db_pcr_backend/controller/GerritController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ic.ac.uk.db_pcr_backend.model.ChangeInfoModel;
+import ic.ac.uk.db_pcr_backend.model.DiffFileModel;
 import ic.ac.uk.db_pcr_backend.model.ProjectInfoModel;
 import ic.ac.uk.db_pcr_backend.service.GerritService;
 
@@ -27,6 +28,15 @@ public class GerritController {
     @GetMapping("/get-changes")
     public List<ChangeInfoModel> getChanges(@RequestParam("q") String query) {
         return gerritService.getChangesWithQuery(query);
+    }
+
+    @GetMapping("/get-modified-files")
+    public List<DiffFileModel> getModifiedFiles(@RequestParam("changeId") String changeId,
+            @RequestParam("revisionId") String revisionId) {
+
+        // TODO: Currently suppose only 1 revision, with revisionId = 1
+
+        return gerritService.getModifiedFileInChange(changeId, "1");
     }
 
     // @GetMapping("/get-commit-list")
