ec6e26e056ee094a3d789ab94ee96448c0227d4e
murixiyang <yangqinxue333@163.com>
2025-04-17 17:46:17 +0800

    backend security

---
+++ backend/db-pcr-backend/src/main/java/ic/ac/uk/db_pcr_backend/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package ic.ac.uk.db_pcr_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+          .authorizeHttpRequests(auth -> auth
+             .anyRequest().authenticated()
+          )
+          .formLogin(form -> form
+             .defaultSuccessUrl("/", true)
+          )
+          .httpBasic(null);
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder pwEnc) {
+        UserDetails user1 = User.withUsername("alice")
+                                .password(pwEnc.encode("alicePass"))
+                                .roles("USER")
+                                .build();
+        UserDetails user2 = User.withUsername("bob")
+                                .password(pwEnc.encode("bobPass"))
+                                .roles("USER")
+                                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
