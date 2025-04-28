159fe7339b6c3a06667af63ece6244713d60fe04
muxi <yangqinxue333@163.com>
2025-04-29 00:40:28 +0100

    Change api constant url

--- backend/db-pcr-backend/src/main/java/ic/ac/uk/db_pcr_backend/Constant.java
+++ backend/db-pcr-backend/src/main/java/ic/ac/uk/db_pcr_backend/Constant.java
@@ -1,8 +1,9 @@
 package ic.ac.uk.db_pcr_backend;
 
 public class Constant {
-    public static final String GERRIT_BASE_URL = "http://gerrit:8080";
-    public static final String GERRIT_AUTHENTICATE_URL = "http://gerrit:8080/a";
+    public static final String GERRIT_BASE_URL = System.getenv().get("GERRIT_URL");
+    public static final String GERRIT_AUTHENTICATE_URL = System.getenv().get("GERRIT_AUTHENTICATE_URL");
+
     public static final String ADMIN_USERNAME = "admin";
     public static final String USERNAME = "murixiyang";
     public static final String ADMIN_PASSWORD = "CwutpZjWKvgnRZf8hPdep8LlpYeu7lIhmZmjxaGVKA";
@@ -10,7 +11,8 @@ public class Constant {
 
     // public static final String GITLAB_API_BASE_URL =
     // "https://gitlab.doc.ic.ac.uk/api/v4";
-    public static final String GITLAB_API_BASE_URL = "http://gitlab/api/v4";
+
+    public static final String GITLAB_API_BASE_URL = System.getenv().get("GITLAB_URL");
     public static final String GITLAB_PERSONAL_TOKEN = "glpat-Smx_zxbc8UJQKLHzZqu6";
 
     public static String getGerritBaseUrl(Boolean needAuth) {
