321bd292defdd96bc6ec83facbdffcd491829fe2
murixiyang <yangqinxue333@163.com>
2025-05-03 21:08:21 +0100

    Correct old path and new path name

--- frontend/db-pcr-frontend/src/app/commit-details/commit-details.component.ts
+++ frontend/db-pcr-frontend/src/app/commit-details/commit-details.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewChecked,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { JsonPipe, NgFor, NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CommitDiffSchema } from '@gitbeaker/rest';
 import { GitlabService } from '../http/gitlab.service';
 import { html } from 'diff2html';
+import { Diff2HtmlUI } from 'diff2html/lib/ui/js/diff2html-ui-slim';
+
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-commit-details',
-  imports: [NgIf, NgFor, FormsModule, JsonPipe],
+  imports: [NgFor, FormsModule],
   templateUrl: './commit-details.component.html',
   styleUrl: './commit-details.component.css',
 })
@@ -21,6 +29,9 @@ export class CommitDetailComponent implements OnInit {
 
   // Store hte generated HTML for the selected file
   diffHtml: SafeHtml | null = null;
+
+  @ViewChild('diffContainer', { static: false })
+  diffContainer!: ElementRef<HTMLElement>;
 
   constructor(
     private route: ActivatedRoute,
@@ -43,24 +54,24 @@ export class CommitDetailComponent implements OnInit {
   }
 
   onSelectFile(diff: CommitDiffSchema) {
-    // 1) Build a minimal unified-diff with headers
+    // Build minimal unified-diff with headers
     const header = [
-      `diff --git a/${diff.old_path} b/${diff.new_path}`,
-      `--- a/${diff.old_path}`,
-      `+++ b/${diff.new_path}`,
+      `diff --git a/${diff['oldPath']} b/${diff['newPath']}`,
+      `--- a/${diff['oldPath']}`,
+      `+++ b/${diff['newPath']}`,
     ].join('\n');
 
     const fullDiff = `${header}\n${diff.diff}`;
 
-    // 1) Generate the raw HTML
-    this.diffHtml = html(fullDiff, {
+    // 1) Draw into your container
+    const ui = new Diff2HtmlUI(this.diffContainer.nativeElement, fullDiff, {
+      outputFormat: 'side-by-side',
       drawFileList: false,
       matching: 'lines',
-      outputFormat: 'side-by-side',
     });
+    ui.draw();
 
-    console.log(diff.diff);
-
-    console.log('HTML:', this.diffHtml);
+    // 2) This wrapper has its own highlight step
+    ui.highlightCode();
   }
 }
