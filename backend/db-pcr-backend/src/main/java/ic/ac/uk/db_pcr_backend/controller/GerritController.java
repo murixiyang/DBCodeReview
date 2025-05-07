7cb6f139d82fa2397dfbd4339e35b485bf7b34fa
murixiyang <yangqinxue333@163.com>
2025-05-07 20:49:27 +0100

    Delete unused api

--- backend/db-pcr-backend/src/main/java/ic/ac/uk/db_pcr_backend/controller/GerritController.java
+++ backend/db-pcr-backend/src/main/java/ic/ac/uk/db_pcr_backend/controller/GerritController.java
@@ -1,26 +1,15 @@
 package ic.ac.uk.db_pcr_backend.controller;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import ic.ac.uk.db_pcr_backend.model.GerritModel.ChangeInfoModel;
-import ic.ac.uk.db_pcr_backend.model.GerritModel.CommentInfoModel;
-import ic.ac.uk.db_pcr_backend.model.GerritModel.CommentInputModel;
-import ic.ac.uk.db_pcr_backend.model.GerritModel.DiffInfoModel;
-import ic.ac.uk.db_pcr_backend.model.GerritModel.FileInfoModel;
-import ic.ac.uk.db_pcr_backend.model.GerritModel.ProjectInfoModel;
 import ic.ac.uk.db_pcr_backend.service.GerritService;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -48,70 +37,6 @@ public class GerritController {
 
         // 3) Return the new Change number to the frontend
         return ResponseEntity.ok(Map.of("changeId", gerritChangeId));
-    }
-
-    @GetMapping("/get-project-list")
-    public Map<String, ProjectInfoModel> getProjectList() {
-        return gerritService.getProjectList();
-    }
-
-    @GetMapping("/get-changes")
-    public List<ChangeInfoModel> getChanges(@RequestParam("q") String query) {
-        return gerritService.getChangesWithQuery(query);
-    }
-
-    @GetMapping("/get-modified-file-list")
-    public Map<String, FileInfoModel> getModifiedFiles(@RequestParam("changeId") String changeId,
-            @RequestParam("revisionId") String revisionId) {
-
-        // TODO: Currently suppose only 1 revision, with revisionId = 1
-
-        return gerritService.getModifiedFileInChange(changeId, revisionId);
-    }
-
-    @GetMapping("/get-file-diff")
-    public DiffInfoModel getDiff(@RequestParam("changeId") String changeId,
-            @RequestParam("revisionId") String revisionId,
-            @RequestParam("filePath") String filePath) {
-
-        // TODO: Currently suppose only 1 revision, with revisionId = 1
-        return gerritService.getDiffInFile(changeId, revisionId, filePath);
-    }
-
-    @GetMapping("/get-draft-comments")
-    public Map<String, CommentInfoModel[]> getDraftComments(@RequestParam("changeId") String changeId,
-            @RequestParam("revisionId") String revisionId) {
-        return gerritService.getAllDraftComments(changeId, revisionId);
-    }
-
-    @GetMapping("/get-comments")
-    public Map<String, CommentInfoModel[]> getComments(@RequestParam("changeId") String changeId,
-            @RequestParam("revisionId") String revisionId) {
-        return gerritService.getAllComments(changeId, revisionId);
-    }
-
-    @PutMapping("/put-draft-comment")
-    public ResponseEntity<CommentInfoModel> putDraftComment(
-            @RequestParam("changeId") String changeId,
-            @RequestParam("revisionId") String revisionId,
-            @RequestBody CommentInputModel commentInput) {
-        return gerritService.putDraftComment(changeId, revisionId, commentInput);
-    }
-
-    @PutMapping("/update-draft-comment")
-    public ResponseEntity<CommentInfoModel> updateDraftComment(
-            @RequestParam("changeId") String changeId,
-            @RequestParam("revisionId") String revisionId,
-            @RequestBody CommentInputModel commentInput) {
-        return gerritService.updateDraftComment(changeId, revisionId, commentInput);
-    }
-
-    @DeleteMapping("/delete-draft-comment")
-    public ResponseEntity<String> deleteDraftComment(
-            @RequestParam("changeId") String changeId,
-            @RequestParam("revisionId") String revisionId,
-            @RequestParam("draftId") String draftId) {
-        return gerritService.deleteDraftComment(changeId, revisionId, draftId);
     }
 
 }
--- backend/db-pcr-backend/src/main/java/ic/ac/uk/db_pcr_backend/service/GerritService.java
+++ backend/db-pcr-backend/src/main/java/ic/ac/uk/db_pcr_backend/service/GerritService.java
@@ -1,19 +1,9 @@
 package ic.ac.uk.db_pcr_backend.service;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -31,20 +21,11 @@ import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.eclipse.jgit.util.ChangeIdUtil;
 import org.gitlab4j.api.models.Project;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gerrit.extensions.api.GerritApi;
 import com.google.gerrit.extensions.api.projects.ProjectInput;
 import com.urswolfer.gerrit.client.rest.GerritAuthData;
 import com.urswolfer.gerrit.client.rest.GerritRestApiFactory;
 import com.urswolfer.gerrit.client.rest.http.HttpStatusException;
-
-import ic.ac.uk.db_pcr_backend.Constant;
-import ic.ac.uk.db_pcr_backend.model.GerritModel.ChangeInfoModel;
-import ic.ac.uk.db_pcr_backend.model.GerritModel.CommentInfoModel;
-import ic.ac.uk.db_pcr_backend.model.GerritModel.CommentInputModel;
-import ic.ac.uk.db_pcr_backend.model.GerritModel.DiffInfoModel;
-import ic.ac.uk.db_pcr_backend.model.GerritModel.FileInfoModel;
-import ic.ac.uk.db_pcr_backend.model.GerritModel.ProjectInfoModel;
 
 @Service
 public class GerritService {
@@ -58,7 +39,6 @@ public class GerritService {
 
     private final GerritAuthData.Basic gerritAuthData;
     private final GerritRestApiFactory gerritApiFactory;
-    private final RestTemplate restTemplate;
 
     public GerritService(
             GitLabService gitLabService,
@@ -78,7 +58,6 @@ public class GerritService {
         this.gerritAuthData = new GerritAuthData.Basic(
                 gerritHttpUrl, gerritUsername, gerritHttpPassword);
         this.gerritApiFactory = new GerritRestApiFactory();
-        this.restTemplate = new RestTemplate();
     }
 
     public String submitForReview(
@@ -208,211 +187,6 @@ public class GerritService {
         // --- 5) Return change id
         return returnChangeId;
 
-    }
-
-    public Map<String, ProjectInfoModel> getProjectList() {
-        String endPoint = "/projects";
-
-        return fetchGerritMapData(endPoint, ProjectInfoModel.class, false);
-    }
-
-    public List<ChangeInfoModel> getChangesWithQuery(String query) {
-        String endPoint = "/changes?q=" + query;
-
-        return fetchGerritListData(endPoint, ChangeInfoModel[].class, false);
-    }
-
-    public Map<String, FileInfoModel> getModifiedFileInChange(String changeId, String revisionId) {
-        String endPoint = "/changes/" + changeId + "/revisions/" + revisionId + "/files";
-
-        return fetchGerritMapData(endPoint, FileInfoModel.class, false);
-    }
-
-    public DiffInfoModel getDiffInFile(String changeId, String revisionId, String filePath) {
-        String endPoint = "/changes/" + changeId + "/revisions/" + revisionId + "/files/" + filePath + "/diff";
-
-        return fetchGerritData(endPoint, DiffInfoModel.class, false);
-    }
-
-    public Map<String, CommentInfoModel[]> getAllDraftComments(String changeId, String revisionId) {
-        String endPoint = "/changes/" + changeId + "/revisions/" + revisionId + "/drafts";
-
-        return fetchGerritMapData(endPoint, CommentInfoModel[].class, true);
-    }
-
-    public Map<String, CommentInfoModel[]> getAllComments(String changeId, String revisionId) {
-        String endPoint = "/changes/" + changeId + "/revisions/" + revisionId + "/comments";
-
-        return fetchGerritMapData(endPoint, CommentInfoModel[].class, false);
-    }
-
-    public ResponseEntity<CommentInfoModel> updateDraftComment(String changeId, String revisionId,
-            CommentInputModel commentInput) {
-
-        String endPoint = Constant.getGerritBaseUrl(true) + "/changes/" + changeId +
-                "/revisions/" + revisionId + "/drafts/" + commentInput.id;
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBasicAuth(Constant.ADMIN_USERNAME, Constant.ADMIN_PASSWORD);
-
-        HttpEntity<CommentInputModel> requestEntity = new HttpEntity<>(commentInput, headers);
-
-        try {
-
-            ResponseEntity<String> response = restTemplate.exchange(
-                    endPoint,
-                    HttpMethod.PUT,
-                    requestEntity,
-                    String.class);
-            String json = CommonFunctionService.trimJson(response.getBody());
-
-            ObjectMapper mapper = new ObjectMapper();
-            CommentInfoModel result = mapper.readValue(json, CommentInfoModel.class);
-
-            return ResponseEntity.status(response.getStatusCode()).body(result);
-
-        } catch (IOException e) {
-            System.out.println("ERROR: Failed to put draft comment to Gerrit at endpoint: " + endPoint);
-            e.printStackTrace();
-            return ResponseEntity.badRequest().build();
-        }
-    }
-
-    public ResponseEntity<CommentInfoModel> putDraftComment(String changeId, String revisionId,
-            CommentInputModel commentInput) {
-
-        String endPoint = Constant.getGerritBaseUrl(true) + "/changes/" + changeId +
-                "/revisions/" + revisionId + "/drafts";
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBasicAuth(Constant.ADMIN_USERNAME, Constant.ADMIN_PASSWORD);
-
-        HttpEntity<CommentInputModel> requestEntity = new HttpEntity<>(commentInput, headers);
-
-        try {
-
-            ResponseEntity<String> response = restTemplate.exchange(
-                    endPoint,
-                    HttpMethod.PUT,
-                    requestEntity,
-                    String.class);
-            String json = CommonFunctionService.trimJson(response.getBody());
-
-            ObjectMapper mapper = new ObjectMapper();
-            CommentInfoModel result = mapper.readValue(json, CommentInfoModel.class);
-
-            return ResponseEntity.status(response.getStatusCode()).body(result);
-
-        } catch (IOException e) {
-            System.out.println("ERROR: Failed to put draft comment to Gerrit at endpoint: " + endPoint);
-            e.printStackTrace();
-            return ResponseEntity.badRequest().build();
-        }
-    }
-
-    public ResponseEntity<String> deleteDraftComment(String changeId, String revisionId, String draftId) {
-        String endPoint = Constant.getGerritBaseUrl(true) + "/changes/" + changeId +
-                "/revisions/" + revisionId + "/drafts/" + draftId;
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBasicAuth(Constant.ADMIN_USERNAME, Constant.ADMIN_PASSWORD);
-
-        HttpEntity<Void> entity = new HttpEntity<>(headers);
-
-        try {
-            ResponseEntity<String> response = restTemplate.exchange(
-                    endPoint,
-                    HttpMethod.DELETE,
-                    entity,
-                    String.class);
-
-            return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
-        } catch (Exception e) {
-            System.out.println("ERROR: Failed to delete draft comment from Gerrit at endpoint: " + endPoint);
-            e.printStackTrace();
-            return ResponseEntity.badRequest().build();
-        }
-    }
-
-    private <T> T fetchGerritData(String endpoint, Class<T> dataClass, Boolean needAuth) {
-        try {
-            String url = Constant.getGerritBaseUrl(needAuth) + endpoint;
-
-            ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-            if (response.getBody() == null) {
-                return null;
-            }
-
-            String json = CommonFunctionService.trimJson(response.getBody());
-
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(json, dataClass);
-        } catch (IOException e) {
-            System.err.println("ERROR: Failed to fetch data from Gerrit at endpoint: " + endpoint);
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private <T> List<T> fetchGerritListData(String endpoint, Class<T[]> dataClass, Boolean needAuth) {
-        try {
-            String url = Constant.getGerritBaseUrl(needAuth) + endpoint;
-
-            HttpHeaders headers = new HttpHeaders();
-            if (needAuth) {
-                headers.setBasicAuth(Constant.ADMIN_USERNAME, Constant.ADMIN_PASSWORD);
-            }
-            HttpEntity<Void> entity = new HttpEntity<>(headers);
-
-            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-            if (response.getBody() == null) {
-                return Collections.emptyList();
-            }
-
-            String json = CommonFunctionService.trimJson(response.getBody());
-
-            System.out.println("json: " + json);
-
-            ObjectMapper mapper = new ObjectMapper();
-            T[] data = mapper.readValue(json, dataClass);
-            return Arrays.asList(data);
-        } catch (IOException e) {
-            System.err.println("ERROR: Failed to fetch data from Gerrit at endpoint: " + endpoint);
-            e.printStackTrace();
-            return Collections.emptyList();
-        }
-    }
-
-    private <T> Map<String, T> fetchGerritMapData(String endpoint, Class<T> dataClass, Boolean needAuth) {
-        try {
-            String url = Constant.getGerritBaseUrl(needAuth) + endpoint;
-
-            HttpHeaders headers = new HttpHeaders();
-            if (needAuth) {
-                headers.setBasicAuth(Constant.ADMIN_USERNAME, Constant.ADMIN_PASSWORD);
-            }
-            HttpEntity<Void> entity = new HttpEntity<>(headers);
-
-            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-            if (response.getBody() == null) {
-                return Collections.emptyMap();
-            }
-
-            String json = CommonFunctionService.trimJson(response.getBody());
-
-            ObjectMapper mapper = new ObjectMapper();
-            Map<String, T> dataMap = mapper.readValue(
-                    json,
-                    mapper.getTypeFactory().constructMapType(Map.class, String.class, dataClass));
-
-            return dataMap;
-        } catch (IOException e) {
-            System.err.println("ERROR: Failed to fetch data from Gerrit at endpoint: " + endpoint);
-            e.printStackTrace();
-            return Collections.emptyMap();
-        }
     }
 
 }
